3a2eadf07c1e2172a18b94a5a24dc48b41fe2b82
Néstor León Vega <neleon@itesi.edu.mx>
2018-06-28 20:34:42 -0500

    Create preguntas.js

---
+++ models/preguntas.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var posibles_valores_respuesta = [1, 2, 3, 4];
+
+var preg_schema = new Schema({
+	pregunta: {type: String, required: "Capture la pregunta"},
+	opcion1: {type: String, required: "Capture la opción 1"},
+	opcion2: {type: String, required: "Capture la opción 2"},
+	opcion3: {type: String, required: "Capture la opción 3"},
+	opcion4: {type: String, required: "Capture la opción 4"},
+	respuesta: {
+		type: Number, 
+		enum: {
+			values: posibles_valores_respuesta, 
+			message: "Opción no válida"
+		}},
+	autor: {type: Schema.Types.ObjectId, ref: "User"} //Relación 1-N
+});
+
+var Pregunta = mongoose.model("Pregunta", preg_schema);
+
+module.exports = Pregunta;
